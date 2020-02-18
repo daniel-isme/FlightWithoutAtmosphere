a2e96829be000ec8d92871bec2423e13b4c2bd86
daniil.melnikov.1999 <daniil.melnikov.1999@mail.ru>
2020-02-18 22:04:41 +0700

    flight with atmosphere simulation

--- FlightWithoutAtmosphere/Form1.Designer.cs
+++ FlightWithoutAtmosphere/Form1.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -44,30 +44,33 @@
             this.text_time = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.label4 = new System.Windows.Forms.Label();
+            this.edit_weight = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.edit_height)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.edit_angle)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.edit_speed)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edit_weight)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea4.AxisX.Maximum = 100D;
-            chartArea4.AxisX.Minimum = 0D;
-            chartArea4.AxisY.Maximum = 50D;
-            chartArea4.AxisY.Minimum = 0D;
-            chartArea4.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea4);
-            legend4.Name = "Legend1";
-            this.chart1.Legends.Add(legend4);
-            this.chart1.Location = new System.Drawing.Point(12, 147);
+            chartArea6.AxisX.Maximum = 100D;
+            chartArea6.AxisX.Minimum = 0D;
+            chartArea6.AxisY.Maximum = 50D;
+            chartArea6.AxisY.Minimum = 0D;
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(12, 159);
             this.chart1.Name = "chart1";
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series4.Legend = "Legend1";
-            series4.Name = "Series1";
-            this.chart1.Series.Add(series4);
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart1.Series.Add(series6);
             this.chart1.Size = new System.Drawing.Size(776, 377);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
@@ -76,7 +79,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(28, 14);
+            this.label1.Location = new System.Drawing.Point(28, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(101, 36);
             this.label1.TabIndex = 1;
@@ -86,7 +89,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(28, 81);
+            this.label2.Location = new System.Drawing.Point(28, 64);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(93, 36);
             this.label2.TabIndex = 2;
@@ -96,7 +99,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(302, 14);
+            this.label3.Location = new System.Drawing.Point(28, 108);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(101, 36);
             this.label3.TabIndex = 3;
@@ -105,7 +108,7 @@
             // edit_height
             // 
             this.edit_height.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.edit_height.Location = new System.Drawing.Point(135, 12);
+            this.edit_height.Location = new System.Drawing.Point(135, 15);
             this.edit_height.Name = "edit_height";
             this.edit_height.Size = new System.Drawing.Size(120, 41);
             this.edit_height.TabIndex = 7;
@@ -113,7 +116,17 @@
             // edit_angle
             // 
             this.edit_angle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.edit_angle.Location = new System.Drawing.Point(135, 79);
+            this.edit_angle.Location = new System.Drawing.Point(135, 62);
+            this.edit_angle.Maximum = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.edit_angle.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.edit_angle.Name = "edit_angle";
             this.edit_angle.Size = new System.Drawing.Size(120, 41);
             this.edit_angle.TabIndex = 8;
@@ -126,7 +139,12 @@
             // edit_speed
             // 
             this.edit_speed.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.edit_speed.Location = new System.Drawing.Point(409, 12);
+            this.edit_speed.Location = new System.Drawing.Point(135, 106);
+            this.edit_speed.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.edit_speed.Name = "edit_speed";
             this.edit_speed.Size = new System.Drawing.Size(120, 41);
             this.edit_speed.TabIndex = 9;
@@ -163,14 +181,24 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.DecimalPlaces = 1;
             this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.numericUpDown1.Increment = new decimal(new int[] {
             1,
             0,
             0,
-            131072});
-            this.numericUpDown1.Location = new System.Drawing.Point(409, 79);
+            65536});
+            this.numericUpDown1.Location = new System.Drawing.Point(411, 62);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 41);
             this.numericUpDown1.TabIndex = 12;
@@ -184,17 +212,53 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(302, 81);
+            this.label4.Location = new System.Drawing.Point(298, 64);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(40, 36);
+            this.label4.Size = new System.Drawing.Size(73, 36);
             this.label4.TabIndex = 13;
-            this.label4.Text = "dt";
+            this.label4.Text = "Size";
+            // 
+            // edit_weight
+            // 
+            this.edit_weight.DecimalPlaces = 1;
+            this.edit_weight.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edit_weight.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.edit_weight.Location = new System.Drawing.Point(411, 18);
+            this.edit_weight.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.edit_weight.Name = "edit_weight";
+            this.edit_weight.Size = new System.Drawing.Size(120, 41);
+            this.edit_weight.TabIndex = 14;
+            this.edit_weight.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(298, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 36);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Weight";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 536);
+            this.ClientSize = new System.Drawing.Size(800, 548);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.edit_weight);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.text_time);
@@ -213,6 +277,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.edit_angle)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.edit_speed)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edit_weight)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -232,6 +297,8 @@
         private System.Windows.Forms.Label text_time;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown edit_weight;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- FlightWithoutAtmosphere/Form1.cs
+++ FlightWithoutAtmosphere/Form1.cs
@@ -18,8 +18,14 @@ namespace FlightWithoutAtmosphere
         }
 
 
-        decimal t, dt = 0.1M, x0, y0, v0, cosa, sina;
+        decimal t, dt = 0.1M, cosa, sina;
+        decimal x, y, v0, S, m, k, vx, vy;
+
+        const decimal g = 9.81M;
+        const decimal C = 0.15M;
+        const decimal rho = 1.29M;
         bool paused = false;
+
 
         private void button_launch_Click(object sender, EventArgs e)
         {
@@ -31,17 +37,20 @@ namespace FlightWithoutAtmosphere
                 button_launch.Text = "Pause";
                 chart1.Series[0].Points.Clear();
                 t = 0;
-                dt = numericUpDown1.Value;
-                x0 = 0;
-                y0 = edit_height.Value;
+                x = 0;
+                y = edit_height.Value;
                 v0 = edit_speed.Value;
 
-                double a = (double)edit_angle.Value * Math.PI / 189;
+                double a = (double)edit_angle.Value * Math.PI / 180;
 
                 cosa = (decimal)Math.Cos(a);
-
                 sina = (decimal)Math.Sin(a);
-                chart1.Series[0].Points.AddXY(x0, y0);
+                S = numericUpDown1.Value;
+                m = edit_weight.Value;
+                k = 0.5M * C * rho * S / m;
+                vx = v0 * cosa;
+                vy = v0 * sina;
+                chart1.Series[0].Points.AddXY(x, y);
                 timer1.Start();
 
             }
@@ -58,14 +67,16 @@ namespace FlightWithoutAtmosphere
             }
         }
 
-        const decimal g = 9.81M;
 
         private void timer1_Tick(object sender, EventArgs e)
         {
 
             t += dt;
-            decimal x = x0 + v0 * cosa * t;
-            decimal y = y0 + v0 * sina * t - g * t * t / 2;
+            decimal v = (decimal)Math.Sqrt((double)(vx * vx + vy * vy));
+            vx = vx - k * vx * v * dt;
+            vy = vy - (g + k * vy * v) * dt;
+            x = x + vx * dt;
+            y = y + vy * dt;
 
             if (x > (decimal)chart1.ChartAreas[0].AxisX.Maximum)
             {
